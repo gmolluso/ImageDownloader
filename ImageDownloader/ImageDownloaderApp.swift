cfff6e54ff1f21d21e27b6efcedc59bcf0dc1ebc
Gustavo Molluso <gmolluso@gmail.com>
2022-05-04 13:19:11 -0300

    Initial Commit

---
+++ ImageDownloader/ImageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloaderApp.swift
+//  ImageDownloader
+//
+//  Created by Gustavo Molluso on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
